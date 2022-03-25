5e9037ce3918e2fffe5f232bc6438646a332d523
leeuihyun <meta212121@naver.com>
2022-03-26 01:52:48 +0900

    auth.js

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,28 +1,76 @@
 import { createAction, handleActions } from "redux-actions";
+import { all, fork, put, takeLatest, delay } from "redux-saga/effects";
+import axios from "axios";
 
-const LOG_IN_REQUEST = "auth/LOG_IN_REQUEST";
-const LOG_IN_SUCCESS = "auth/LOG_IN_SUCCESS";
-const LOG_IN_FAILURE = "auth/LOG_IN_FAILURE";
+const LOG_IN_REQUEST = "LOG_IN_REQUEST";
+const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+const LOG_IN_FAILURE = "LOG_IN_FAILURE";
 
-const LOG_OUT_REQUEST = "auth/LOG_OUT_REQUEST";
-const LOG_OUT_SUCCESS = "auth/LOG_OUT_SUCCESS";
-const LOG_OUT_FAILURE = "auth/LOG_OUT_FAILURE";
+const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
-const REGISTER_REQUEST = "auth/REGISTER_REQUEST";
-const REGISTER_SUCCESS = "auth/REGISTER_SUCCESS";
-const REGISTER_FAILURE = "auth/REGISTER_FAILURE";
+const REGISTER_REQUEST = "REGISTER_REQUEST";
+const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+const REGISTER_FAILURE = "REGISTER_FAILURE";
 
 export const logInRequest = createAction(LOG_IN_REQUEST, (data) => data);
 export const logOutRequest = createAction(LOG_OUT_REQUEST);
 export const registerRequest = createAction(REGISTER_REQUEST, (data) => data);
 
+function logInApi(data) {
+    return axios.post("/logIn/api", data);
+}
+function logOutApi(data) {
+    return axios.post("logOut/api");
+}
+
+function* logIn(action) {
+    try {
+        //const res = yield call(logInApi, action.payload);
+        yield delay(1000);
+        yield put({
+            type: LOG_IN_SUCCESS,
+            payload: action.payload,
+        });
+        //console.log("LOG_IN_SAGA_SUCCESS");
+    } catch (err) {
+        console.log(err);
+        yield put({
+            type: LOG_IN_FAILURE,
+            payload: err.response.data,
+        });
+    }
+}
+
+function* logOut(action) {
+    try {
+        //const res = yield call(logOutApi, action.payload);
+        yield delay(1000);
+        yield put({
+            type: LOG_OUT_SUCCESS,
+            //payload : res.data
+        });
+    } catch (err) {
+        console.log(err);
+        yield put({
+            type: LOG_OUT_FAILURE,
+        });
+    }
+}
+
+export function* authSaga() {
+    yield takeLatest(LOG_IN_REQUEST, logIn);
+    yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+//
 const initialState = {
     isLoggedIn: false,
     isLoggingIn: false,
     isLoggingOut: false,
     user: null,
     registerData: {},
-    loginData: {},
+    logInData: {},
     studentList: [
         {
             name: "mrlee",
@@ -39,9 +87,8 @@ const auth = handleActions(
     {
         [LOG_IN_REQUEST]: (state, action) => ({
             ...state,
-            isLoggedIn: false,
             isLoggingIn: true,
-            user: action.payload, //나중에 바꿔야함 테스트하기 위함 (saga 이전)
+            //user: action.payload, //나중에 바꿔야함 테스트하기 위함 (saga 이전)
         }),
         [LOG_IN_SUCCESS]: (state, action) => ({
             ...state,
@@ -49,19 +96,19 @@ const auth = handleActions(
             isLoggingIn: false,
             user: action.payload,
         }),
-        [LOG_IN_FAILURE]: (state, action) => ({
+        [LOG_IN_FAILURE]: (state) => ({
             ...state,
-            isLoggedIn: false,
             isLoggingIn: false,
         }),
         [LOG_OUT_REQUEST]: (state) => ({
             ...state,
             isLoggingOut: true,
         }),
-        [LOG_OUT_SUCCESS]: (state) => ({
+        [LOG_OUT_SUCCESS]: (state, action) => ({
             ...state,
+            isLoggedIn: false,
             isLoggingOut: false,
-            user: null,
+            user: action.payload,
         }),
         [LOG_OUT_FAILURE]: (state) => ({
             ...state,
