046e4f7ac8dbc5462e05eb4741efde1612d3eff6
leeuihyun <meta212121@naver.com>
2022-03-26 01:53:49 +0900

    index.js saga setting

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./reducers";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
