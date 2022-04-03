4e9d70c15a2d4df2818464b4562a881157368c07
leeuihyun <meta212121@naver.com>
2022-04-03 23:08:25 +0900

    StudentRegister form

---
+++ src/components/StudentRegisterForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button, Form } from "antd";
+//학생 이름 나이  등등
+function StudentRegisterForm() {
+    return <div>StudentRegisterForm</div>;
+}
+
+export default StudentRegisterForm;
