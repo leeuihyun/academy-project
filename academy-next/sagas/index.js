5f7f336e36fcd411166d88d02b3d5815d37879fe
leeuihyun <meta212121@naver.com>
2022-05-02 17:17:54 +0900

    index.js saga

--- academy-next/sagas/index.js
+++ academy-next/sagas/index.js
@@ -1,5 +1,9 @@
 import { takeLatest, fork, all } from "redux-saga/effects";
 import userSaga from "./user";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080/users";
+axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
     yield all([fork(userSaga)]);
